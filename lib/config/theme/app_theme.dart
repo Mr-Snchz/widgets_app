be8d83463f839a66dcf92dc0c028b2d2191311be
Mario Sanchez <luis431254@gmail.com>
2026-09-23 08:57:44 -0600

    Se agrego Theme al proyecto

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+
+
+import 'package:flutter/material.dart';
+
+
+const colorList = <Color>[
+  Colors.black,
+  Colors.blue,
+  Colors.red,
+  Colors.amber,
+  Colors.green, 
+  Colors.purple
+];
+
+class AppTheme {
+  
+  final int selectColor ;
+
+  AppTheme({
+    required this.selectColor
+  }): assert(selectColor >= 0 , 'El color seleccionado es menor a 0 '),
+      assert( selectColor <= colorList.length - 1, 'El color seleccionado es mayor a el numero del arreglo'); 
+
+  ThemeData getThemeData () => ThemeData(
+    colorSchemeSeed: colorList[selectColor]
+  );
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'config/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectColor: 0).getThemeData(),
+      home: Scaffold(body: 
+      Center(
+        child: FilledButton(onPressed: () {  },
+        child: Text('Hello World!'))
+        )
+      ),
     );
   }
 }
