13ebeb2cda31e9688aee9aa9c2363371bb7b6151
Mario Sanchez <luis431254@gmail.com>
2026-09-23 22:26:18 -0600

    HomeScreen Implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_item.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -11,6 +12,48 @@ class HomeScreen extends StatelessWidget {
         title: Text('Home Screen '),
         centerTitle: false,
       ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    
+
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (BuildContext context, int index) {
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      }
+      );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const new({
+    required this.menuItem,
+  });
+  
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary,),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subtitle),
+      onTap: () {
+        //TODO: NAVEGAR A OTRA PANTALLA 
+      },
     );
   }
 }
